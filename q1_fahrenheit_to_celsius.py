02d2f2185d9e05d764e0128df5f00a56c7d597f1
kohyt <koh.yiting@dhs.sg>
2016-01-18 12:34:26 +0800

    Create q1_fahrenheit_to_celsius.py

---
+++ q1_fahrenheit_to_celsius.py
@@ -0,0 +1,11 @@
+# q1_fahrenheit_to_celsius.py
+# Fahrenheit to Celsius converter
+
+# get input
+fahrenheit = float(input("Enter Fahrenheit temperature:"))
+
+# convert temperature
+celsius = (5/9)*(fahrenheit-32)
+
+# output result
+print("Temperature in Celsius is", celsius)
